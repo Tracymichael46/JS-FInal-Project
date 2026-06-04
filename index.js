97e014754b5cbbd32bf86417e35d9b7610b20584
Tracymichael46 <tracymichael45@gmail.com>
2026-06-04 09:16:06 -0400

    update 1

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 // http://www.omdbapi.com/?i=tt3896198&apikey=55655026
+let isMenuOpen = false;
 
 function togglemenu() {
-    
+    if (isMenuOpen) {
+        isMenuOpen = false;
+        return document.body.classList.remove("menu__open")
+    }
+    isMenuOpen = true;
+    document.body.clasList += ' menu__open'
 }
